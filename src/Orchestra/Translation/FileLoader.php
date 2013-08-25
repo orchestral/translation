065e4acb749c269db0d8979d1235bb5c3be7c485
crynobone <crynobone@gmail.com>
2013-08-25 16:39:56 +0800

    Add cascading filesystem support for translation files
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Translation/FileLoader.php
+++ src/Orchestra/Translation/FileLoader.php
@@ -1,5 +1,22 @@
 <?php namespace Orchestra\Translation;
 
 class FileLoader extends \Illuminate\Translation\FileLoader {
-	
+
+	/**
+	 * Load a namespaced translation group.
+	 *
+	 * @param  string  $locale
+	 * @param  string  $group
+	 * @param  string  $namespace
+	 * @return array
+	 */
+	protected function loadNamespaced($locale, $group, $namespace)
+	{
+		if ($this->files->exists($full = "{$this->path}/{$locale}/packages/{$namespace}/{$group}.php"))
+		{
+			return $this->files->getRequire($full);
+		}
+
+		return parent::loadNamespaced($locale, $group, $namespace);
+	}
 }
--- tests/FileLoaderTest.php
+++ tests/FileLoaderTest.php
@@ -26,4 +26,32 @@ class FileLoaderTest extends \PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf('Illuminate\Translation\FileLoader', $stub);
 		$this->assertInstanceOf('Illuminate\Translation\LoaderInterface', $stub);
 	}
+
+	/**
+	 * Test Orchestra\Translation\FileLoader::loadNamespaced() method.
+	 *
+	 * @test
+	 */
+	public function testLoadNamespacedMethod()
+	{
+		$path  = '/var/app/lang';
+		$files = m::mock('Illuminate\Filesystem\Filesystem');
+		$stub  = new FileLoader($files, $path);
+
+		$stub->addNamespace('orchestra/foundation', '/var/vendor/orchestra/foundation/src/lang');
+
+		$files->shouldReceive('exists')->once()
+				->with("{$path}/en/packages/orchestra/foundation/install.php")->andReturn(true)
+			->shouldReceive('getRequire')->once()
+				->with("{$path}/en/packages/orchestra/foundation/install.php")->andReturn(array('install' => 'Install'))
+			->shouldReceive('exists')->once()
+				->with("{$path}/en/packages/orchestra/foundation/web.php")->andReturn(false)
+			->shouldReceive('exists')->once()
+				->with("/var/vendor/orchestra/foundation/src/lang/en/web.php")->andReturn(true)
+			->shouldReceive('getRequire')->once()
+				->with("/var/vendor/orchestra/foundation/src/lang/en/web.php")->andReturn(array('web' => 'Web'));
+
+		$this->assertEquals(array('install' => 'Install'), $stub->load('en', 'install', 'orchestra/foundation'));
+		$this->assertEquals(array('web' => 'Web'), $stub->load('en', 'web', 'orchestra/foundation'));
+	}
 }
