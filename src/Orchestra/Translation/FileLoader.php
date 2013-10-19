520bd8889bc811e9e61b6acf2436bd4693ee6ca5
crynobone <crynobone@gmail.com>
2013-10-19 19:13:08 +0800

    Implement PSR-2 code standard.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Translation/FileLoader.php
+++ src/Orchestra/Translation/FileLoader.php
@@ -1,32 +1,31 @@
 <?php namespace Orchestra\Translation;
 
-class FileLoader extends \Illuminate\Translation\FileLoader {
+class FileLoader extends \Illuminate\Translation\FileLoader
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function loadNamespaced($locale, $group, $namespace)
+    {
+        $items = parent::loadNamespaced($locale, $group, $namespace);
+        $file  = "{$this->path}/{$locale}/packages/{$namespace}/{$group}.php";
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function loadNamespaced($locale, $group, $namespace)
-	{
-		$items = parent::loadNamespaced($locale, $group, $namespace);
-		$file  = "{$this->path}/{$locale}/packages/{$namespace}/{$group}.php";
+        if ($this->files->exists($file)) {
+            $items = $this->mergeEnvironment($items, $file);
+        }
 
-		if ($this->files->exists($file))
-		{
-			$items = $this->mergeEnvironment($items, $file);
-		}
+        return $items;
+    }
 
-		return $items;
-	}
-
-	/**
-	 * Merge the items in the given file into the items.
-	 *
-	 * @param  array   $items
-	 * @param  string  $file
-	 * @return array
-	 */
-	protected function mergeEnvironment(array $items, $file)
-	{
-		return array_replace_recursive($items, $this->files->getRequire($file));
-	}
+    /**
+     * Merge the items in the given file into the items.
+     *
+     * @param  array   $items
+     * @param  string  $file
+     * @return array
+     */
+    protected function mergeEnvironment(array $items, $file)
+    {
+        return array_replace_recursive($items, $this->files->getRequire($file));
+    }
 }
