446cf1cd8966a8d23c71a63b20a637a3389dca39
crynobone <crynobone@gmail.com>
2014-12-30 10:41:23 +0800

    Add BrowserLocale middleware.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Middleware/BrowserLocale.php
@@ -0,0 +1,39 @@
+<?php namespace Orchestra\Translation\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Contracts\Foundation\Application;
+
+class BrowserLocale implements Middleware
+{
+    /**
+     * The application implementation.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->app->setLocale($request->getPreferredLanguage());
+
+        return $next($request);
+    }
+}
